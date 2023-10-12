e1ceff98bd6891f6aa284259ca8b6a06291e9f8f
Denis Durbalov <den.dyrbalov25@gmail.com>
2023-10-12 11:22:46 +0200

    leetCode problems

--- src/main/java/org/example/LeetCodeProblems/HashMap/HashMapProblems.java
+++ src/main/java/org/example/LeetCodeProblems/HashMap/HashMapProblems.java
@@ -6,9 +6,17 @@ import java.util.Map;
 
 public class HashMapProblems {
     public static void main(String[] args) {
-        String ransomNote = "a";
-        String magazine = "d";
-//        System.out.println(canConstruct(ransomNote, magazine));
+        String s = "anagram";
+        String t = "nagaram";
+
+        String s1 = "rat";
+        String t1 = "car";
+
+        String s2 = "anagram";
+        String t2 = "nagaram";
+
+
+        System.out.println(isAnagram(s1, t1));
     }
 
     public static boolean wordPattern(String pattern, String s) {
@@ -72,6 +80,55 @@ public class HashMapProblems {
             else if (ransomNoteMap.get(entry.getKey()) > magazineMap.get(entry.getKey())) {
                 return false;
             }
+        }
+        return true;
+    }
+
+    public static boolean isIsomorphic(String s, String t) {
+        HashMap<Character, Character> mapperIsomorphic = new HashMap<>();
+        for (int x = 0; x < s.length(); x++) {
+            if (mapperIsomorphic.containsValue(t.charAt(x)) || mapperIsomorphic.containsKey(s.charAt(x))) {
+                if (!mapperIsomorphic.containsKey(s.charAt(x))) return false;
+                else if (!mapperIsomorphic.get(s.charAt(x)).equals(t.charAt(x))) return false;
+            } else {
+                mapperIsomorphic.put(s.charAt(x), t.charAt(x));
+            }
+        }
+        return true;
+    }
+
+    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int x = 0; x < nums.length; x++) {
+            if (map.containsKey(nums[x])) {
+                if (Math.abs(map.get(nums[x]) - x) <= k) return true;
+            }
+            map.put(nums[x], x);
+        }
+        return false;
+    }
+
+//    Example 1:
+//
+//    Input: s = "anagram", t = "nagaram"
+//    Output: true
+//    Example 2:
+//
+//    Input: s = "rat", t = "car"
+//    Output: false
+
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        HashMap<Character, Integer> mapLetter = new HashMap<>();
+        for (int x = 0; x < s.length(); x++) {
+            if (mapLetter.containsKey(s.charAt(x))) mapLetter.put(s.charAt(x), mapLetter.get(s.charAt(x)) + 1);
+            else mapLetter.put(s.charAt(x), 1);
+        }
+
+        for (int x = 0; x < t.length(); x++) {
+            if (!mapLetter.containsKey(t.charAt(x))) return false;
+            else if (mapLetter.get(t.charAt(x)) - 1 == -1) return false;
+            mapLetter.put(t.charAt(x), mapLetter.get(t.charAt(x)) - 1);
         }
         return true;
     }
